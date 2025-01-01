c308024d4c9454de4dd9699ff305f75fc23fa1ff
Asish Kumar <officialasishkumar@gmail.com>
2025-01-01 15:25:56 +0530

    add frontend url
    Signed-off-by: Asish Kumar <officialasishkumar@gmail.com>

--- app/config.py
+++ app/config.py
@@ -6,5 +6,6 @@ load_dotenv()
 class Settings:
     SQLALCHEMY_DATABASE_URL: str = os.getenv("SQLALCHEMY_DATABASE_URL", "")
     SECRET_KEY: str = os.getenv("SECRET_KEY", "")
+    FRONTEND_URL: str = os.getenv("FRONTEND_URL", "")
 
 settings = Settings()
